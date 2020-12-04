cd950d83254ae6a1572403f1ccb1199ca99edd4b
Divyansh Kumar Pathak <42887919+Divyansh-Pathak@users.noreply.github.com>
2020-12-04 13:11:47 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,185 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true});
+
+const userSchema = new mongoose.Schema{
+  fName: String,
+  lName: String,
+  eMail: String,
+  password: String
+}
+
+const User = mongoose.model("User", userSchema);
+
+const articleSchema = {
+  title: String,
+  content: String,
+  comment: {
+    comment: String,
+    user: String
+  }
+};
+
+const Article = mongoose.model("Article", articleSchema);
+
+
+app.get("/login", function(req, res){
+  res.render("login");
+});
+
+app.get("/register", function(req, res){
+  res.render("register");
+});
+
+app.post("/register", function(req, res){
+  const newUser =  new User({
+    fName: req.body.fName,
+    lName: req.body.lName,
+    eMail: req.body.username,
+    password: req.body.password
+  });
+  newUser.save(function(err){
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+});
+
+app.post("/login", function(req, res){
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({eMail: username}, function(err, foundUser){
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render("secrets");
+        }
+      }
+    }
+  });
+});
+
+
+///////////////////////////////////Requests Targetting all Articles////////////////////////
+
+app.route("/articles")
+
+.get(function(req, res){
+  Article.find(function(err, foundArticles){
+    if (!err) {
+      res.send(foundArticles);
+    } else {
+      res.send(err);
+    }
+  });
+})
+
+.post(function(req, res){
+
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content
+  });
+
+  newArticle.save(function(err){
+    if (!err){
+      res.send("Successfully added a new article.");
+    } else {
+      res.send(err);
+    }
+  });
+})
+
+.delete(function(req, res){
+
+  Article.deleteMany(function(err){
+    if (!err){
+      res.send("Successfully deleted all articles.");
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+////////////////////////////////Requests Targetting A Specific Article////////////////////////
+
+app.route("/articles/:articleTitle")
+
+.get(function(req, res){
+
+  Article.findOne({title: req.params.articleTitle}, function(err, foundArticle){
+    if (foundArticle) {
+      res.send(foundArticle);
+    } else {
+      res.send("No articles matching that title was found.");
+    }
+  });
+})
+
+.put(function(req, res){
+
+  Article.update(
+    {title: req.params.articleTitle},
+    {title: req.body.title, content: req.body.content},
+    {overwrite: true},
+    function(err){
+      if(!err){
+        res.send("Successfully updated the selected article.");
+      }
+    }
+  );
+})
+
+.patch(function(req, res){
+
+  Article.update(
+    {title: req.params.articleTitle},
+    {$set: req.body},
+    function(err){
+      if(!err){
+        res.send("Successfully updated article.");
+      } else {
+        res.send(err);
+      }
+    }
+  );
+})
+
+.delete(function(req, res){
+
+  Article.deleteOne(
+    {title: req.params.articleTitle},
+    function(err){
+      if (!err){
+        res.send("Successfully deleted the corresponding article.");
+      } else {
+        res.send(err);
+      }
+    }
+  );
+});
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
